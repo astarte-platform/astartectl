130fc16b64a890844f559470f4a3482a795f0ab0
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-08-06 15:13:36 +0200

    root: add astarte-url persistent flag

--- cmd/root.go
+++ cmd/root.go
@@ -53,6 +53,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.astartectl.yaml)")
+	rootCmd.PersistentFlags().StringP("astarte-url", "u", "", "Base url for your Astarte deployment (e.g. https://api.astarte.example.com)")
 }
 
 // initConfig reads in config file and ENV variables if set.
