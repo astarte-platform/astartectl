b87a418f8da50f31d99abd55cfe380003259a4d2
Riccardo Binetti <riccardo.binetti@ispirata.com>
2019-09-25 16:28:57 +0200

    utils: add gen-jwt command
    Signed-off-by: Riccardo Binetti <riccardo.binetti@ispirata.com>

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -15,6 +15,7 @@
 package utils
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 
@@ -23,8 +24,12 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
+	"time"
 )
 
 // utilsCmd represents the utils command
@@ -53,9 +58,40 @@ var genDeviceIdCmd = &cobra.Command{
 	RunE:    genDeviceIdF,
 }
 
+var jwtTypesToClaim = map[string]string{
+	"housekeeping":     "a_ha",
+	"realm-management": "a_rma",
+	"pairing":          "a_pa",
+	"appengine":        "a_aea",
+	"channels":         "a_ch",
+}
+
+var jwtTypes = []string{"housekeeping", "realm-management", "pairing", "appengine", "channels"}
+
+var genJwtCmd = &cobra.Command{
+	Use:       "gen-jwt <type>",
+	Short:     "Generate a JWT",
+	Long:      `Generate a JWT to access one of astarte APIs.`,
+	Example:   `  astartectl utils gen-jwt realm-management -p test-realm.key`,
+	Args:      cobra.ExactArgs(1),
+	ValidArgs: jwtTypes,
+	RunE:      genJwtF,
+}
+
+var defaultClaims = []string{".*::.*"}
+
 func init() {
+	genJwtCmd.Flags().StringP("private-key", "p", "", `Path to PEM encoded private key.
+Should be Housekeeping key to generate an housekeeping token, Realm key for everything else.`)
+	genJwtCmd.MarkFlagRequired("private-key")
+	genJwtCmd.MarkFlagFilename("private-key")
+	genJwtCmd.Flags().StringSliceP("claims", "c", defaultClaims, `The list of claims to be added in the JWT. Defaults to .*::.* (i.e. all-access)
+You can specify the flag multiple times or separate the claims with a comma.`)
+	genJwtCmd.Flags().Int64P("expiry", "e", 300, "Expiration time of the token in seconds. 0 means the token will never expire.")
+
 	UtilsCmd.AddCommand(genKeypairCmd)
 	UtilsCmd.AddCommand(genDeviceIdCmd)
+	UtilsCmd.AddCommand(genJwtCmd)
 }
 
 func genKeypairF(command *cobra.Command, args []string) error {
@@ -88,6 +124,74 @@ func genDeviceIdF(command *cobra.Command, args []string) error {
 	checkError(err)
 
 	fmt.Println(base64.RawURLEncoding.EncodeToString(uuidBytes))
+
+	return nil
+}
+
+func validJwtType(t string) bool {
+	for _, validType := range jwtTypes {
+		if t == validType {
+			return true
+		}
+	}
+	return false
+}
+
+func genJwtF(command *cobra.Command, args []string) error {
+	jwtType := args[0]
+	if !validJwtType(jwtType) {
+		errorString := fmt.Sprintf("Invalid type. Valid types are: %s", strings.Join(jwtTypes, ", "))
+
+		return errors.New(errorString)
+	}
+
+	privateKey, err := command.Flags().GetString("private-key")
+	if err != nil {
+		return err
+	}
+
+	keyPEM, err := ioutil.ReadFile(privateKey)
+	if err != nil {
+		return err
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(keyPEM)
+	if err != nil {
+		return err
+	}
+
+	accessClaims, err := command.Flags().GetStringSlice("claims")
+	if err != nil {
+		return err
+	}
+
+	accessClaimKey := jwtTypesToClaim[jwtType]
+
+	now := time.Now().UTC().Unix()
+	claims := jwt.MapClaims{
+		"iat": now,
+	}
+
+	claims[accessClaimKey] = accessClaims
+
+	expiryOffset, err := command.Flags().GetInt64("expiry")
+	if err != nil {
+		return err
+	}
+
+	if expiryOffset != 0 {
+		expiry := now + expiryOffset
+		claims["expiry"] = expiry
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(tokenString)
 
 	return nil
 }
