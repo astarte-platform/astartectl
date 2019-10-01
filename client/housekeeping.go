81ead07bfa598a0210a9abf9427beea5a39f3179
Dario Freddi <dario.freddi@ispirata.com>
2019-10-01 23:24:21 +0200

    client/housekeeping: Implement Housekeeping API Service
    Signed-off-by: Dario Freddi <dario.freddi@ispirata.com>

--- client/housekeeping.go
+++ client/housekeeping.go
@@ -14,10 +14,95 @@
 
 package client
 
-import "net/url"
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"path"
+)
 
 // HousekeepingService is the API Client for Housekeeping API
 type HousekeepingService struct {
 	client          *Client
 	housekeepingURL *url.URL
+}
+
+// ListRealms returns all realms in the cluster.
+func (s *HousekeepingService) ListRealms(token string) ([]string, error) {
+	callURL, _ := url.Parse(s.housekeepingURL.String())
+	callURL.Path = path.Join(callURL.Path, "/v1/realms")
+	decoder, err := s.client.genericJSONDataAPIGET(callURL.String(), token, 200)
+	if err != nil {
+		return nil, err
+	}
+	var responseBody struct {
+		Data []string `json:"data"`
+	}
+	err = decoder.Decode(&responseBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseBody.Data, nil
+}
+
+// GetRealm returns data about a single Realm.
+func (s *HousekeepingService) GetRealm(realm string, token string) (map[string]interface{}, error) {
+	callURL, _ := url.Parse(s.housekeepingURL.String())
+	callURL.Path = path.Join(callURL.Path, fmt.Sprintf("/v1/realms/%s", realm))
+	decoder, err := s.client.genericJSONDataAPIGET(callURL.String(), token, 200)
+	if err != nil {
+		return nil, err
+	}
+	var responseBody struct {
+		Data map[string]interface{} `json:"data"`
+	}
+	err = decoder.Decode(&responseBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseBody.Data, nil
+}
+
+// CreateRealm creates a new Realm in the Cluster with default parameters.
+func (s *HousekeepingService) CreateRealm(realm string, publicKeyString string, token string) error {
+	return s.createRealmInternal(realm, publicKeyString, 0, nil, token)
+}
+
+// CreateRealmWithReplicationFactor creates a new Realm in the Cluster with a custom Replication Factor.
+// The replication factor must always be > 0.
+func (s *HousekeepingService) CreateRealmWithReplicationFactor(realm string, publicKeyString string,
+	replicationFactor int, token string) error {
+	if replicationFactor <= 0 {
+		return errors.New("Replication factor should be > 0")
+	}
+	return s.createRealmInternal(realm, publicKeyString, replicationFactor, nil, token)
+}
+
+// CreateRealmWithDatacenterReplication creates a new Realm in the Cluster with a custom,
+// per-datacenter Replication Factor. Both replicationClass and datacenterReplicationFactors must be provided.
+func (s *HousekeepingService) CreateRealmWithDatacenterReplication(realm string, publicKeyString string,
+	datacenterReplicationFactors map[string]int, token string) error {
+	return s.createRealmInternal(realm, publicKeyString, 0, datacenterReplicationFactors, token)
+}
+
+func (s *HousekeepingService) createRealmInternal(realm string, publicKeyString string, replicationFactor int,
+	datacenterReplicationFactors map[string]int, token string) error {
+	callURL, _ := url.Parse(s.housekeepingURL.String())
+	callURL.Path = path.Join(callURL.Path, "/v1/realms")
+
+	requestBody := map[string]interface{}{
+		"realm_name":         realm,
+		"jwt_public_key_pem": publicKeyString,
+	}
+
+	if replicationFactor > 0 {
+		requestBody["replication_factor"] = replicationFactor
+	} else if datacenterReplicationFactors != nil {
+		requestBody["replication_class"] = "NetworkTopologyStrategy"
+		requestBody["datacenter_replication_factors"] = datacenterReplicationFactors
+	}
+
+	return s.client.genericJSONDataAPIPost(callURL.String(), requestBody, token, 201)
 }
