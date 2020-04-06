7ae7d055fc38b03bd16ba005e43152f8f62d30e2
Dario Freddi <dario.freddi@ispirata.com>
2020-04-06 07:52:55 +0200

    housekeeping/realms: Use new syntax to integrate with context
    realms create now works with a completely different syntax, which allows feeding
    either a private key, or no key at all. This deeply integrates with the context
    mechanism, allowing to immediately switch to the new realm context upon realm
    creation, in a similar fashion to what public cloud command lines do.
    
    Signed-off-by: Dario Freddi <dario.freddi@ispirata.com>

--- cmd/housekeeping/realms.go
+++ cmd/housekeeping/realms.go
@@ -15,14 +15,26 @@
 package housekeeping
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"strconv"
 	"strings"
+	"text/tabwriter"
 
+	"github.com/astarte-platform/astartectl/config"
+	"github.com/astarte-platform/astartectl/utils"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // realmsCmd represents the realms command
@@ -63,9 +75,16 @@ var realmsCreateCmd = &cobra.Command{
 func init() {
 	HousekeepingCmd.AddCommand(realmsCmd)
 
-	realmsCreateCmd.Flags().StringP("public-key", "p", "", "Path to PEM encoded public key used as realm key")
-	realmsCreateCmd.MarkFlagRequired("public-key")
-	realmsCreateCmd.MarkFlagFilename("public-key")
+	realmsCreateCmd.Flags().String("realm-private-key", "", "Path to PEM encoded public key used as realm key")
+	if err := realmsCreateCmd.MarkFlagFilename("realm-private-key"); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	realmsCreateCmd.Flags().String("realm-public-key", "", "Path to PEM encoded public key used as realm key")
+	if err := realmsCreateCmd.MarkFlagFilename("realm-public-key"); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 	realmsCreateCmd.Flags().IntP("replication-factor", "r", 0, `Replication factor for the realm, used with SimpleStrategy replication.`)
 	realmsCreateCmd.Flags().StringSliceP("datacenter-replication", "d", nil,
 		`Replication factor for a datacenter, used with NetworkTopologyStrategy replication.
@@ -106,14 +125,16 @@ func realmsShowF(command *cobra.Command, args []string) error {
 
 func realmsCreateF(command *cobra.Command, args []string) error {
 	realm := args[0]
-	publicKey, err := command.Flags().GetString("public-key")
+	publicKey, err := command.Flags().GetString("realm-public-key")
 	if err != nil {
 		return err
 	}
-
-	publicKeyContent, err := ioutil.ReadFile(publicKey)
+	privateKey, err := command.Flags().GetString("realm-private-key")
 	if err != nil {
 		return err
+	}
+	if privateKey != "" && publicKey != "" {
+		return errors.New("when passing --realm-private-key, --realm-public-key should not be specified")
 	}
 
 	replicationFactor, err := command.Flags().GetInt("replication-factor")
@@ -130,10 +151,17 @@ func realmsCreateF(command *cobra.Command, args []string) error {
 		return errors.New("replication-factor and datacenter-replication are mutually exclusive, you only have to specify one")
 	}
 
-	if replicationFactor > 0 {
-		err = astarteAPIClient.Housekeeping.CreateRealmWithReplicationFactor(realm, string(publicKeyContent), replicationFactor)
-	} else if len(datacenterReplications) > 0 {
-		datacenterReplicationFactors := make(map[string]int)
+	createContext := true
+	clusterConfigurationName, err := getClusterNameFromURLs()
+	if err != nil {
+		createContext = false
+	}
+	if privateKey == "" && publicKey != "" {
+		createContext = false
+	}
+
+	datacenterReplicationFactors := make(map[string]int)
+	if len(datacenterReplications) > 0 {
 		for _, datacenterString := range datacenterReplications {
 			tokens := strings.Split(datacenterString, ":")
 			if len(tokens) != 2 {
@@ -148,6 +176,106 @@ func realmsCreateF(command *cobra.Command, args []string) error {
 			}
 			datacenterReplicationFactors[datacenter] = datacenterReplicationFactor
 		}
+	}
+
+	urlString := viper.GetString("url")
+	if viper.GetString("individual-urls.housekeeping") != "" {
+		urlString = viper.GetString("individual-urls.housekeeping")
+	}
+	astarteURL, err := url.Parse(urlString)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	contextName := ""
+	if createContext {
+		contextName = fmt.Sprintf("%s-realm-%s", astarteURL.Hostname(), realm)
+	}
+
+	fmt.Println("Will create Astarte Realm with following parameters:")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+	fmt.Fprintf(w, "Astarte Cluster API Host:\t%s\n", astarteURL.Hostname())
+	fmt.Fprintf(w, "Realm name:\t%s\n", realm)
+	if len(datacenterReplicationFactors) > 0 {
+		fmt.Fprint(w, "Datacenter replications:")
+		for k, v := range datacenterReplicationFactors {
+			fmt.Fprintf(w, "\t%s: %d\n", k, v)
+		}
+	} else {
+		printedReplicationFactor := replicationFactor
+		if replicationFactor == 0 {
+			printedReplicationFactor = 1
+		}
+		fmt.Fprintf(w, "Replication factor:\t%d\n", printedReplicationFactor)
+	}
+	if createContext {
+		fmt.Fprintf(w, "Astarte Context:\t%s\n", contextName)
+	}
+	w.Flush()
+	fmt.Println()
+
+	if !createContext {
+		fmt.Println("Will not create an Astarte context - to do so, you need to have a matching Astarte Cluster configuration and supply a private key for the Realm.")
+		fmt.Println()
+	}
+
+	if privateKey == "" && publicKey == "" {
+		fmt.Println("A new private key will be generated for this realm and saved in your configuration.")
+		fmt.Printf("You will be able to access it with \"astartectl config contexts get-realm-key %s\".\n", contextName)
+		fmt.Println()
+	}
+
+	if ok, err := utils.AskForConfirmation("Do you want to continue?"); !ok || err != nil {
+		os.Exit(0)
+	}
+
+	var publicKeyContent []byte
+	var privateKeyContent []byte
+	switch {
+	case publicKey != "":
+		publicKeyContent, err = ioutil.ReadFile(publicKey)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	case privateKey != "":
+		privateKeyContent, err = ioutil.ReadFile(privateKey)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		key, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyContent)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if publicKeyContent, err = getPublicKeyPEMBytes(&key.PublicKey); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	default:
+		reader := rand.Reader
+		bitSize := 4096
+		key, err := rsa.GenerateKey(reader, bitSize)
+		if err != nil {
+			return err
+		}
+
+		if privateKeyContent, err = getPrivateKeyPEMBytes(key); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if publicKeyContent, err = getPublicKeyPEMBytes(&key.PublicKey); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if replicationFactor > 0 {
+		err = astarteAPIClient.Housekeeping.CreateRealmWithReplicationFactor(realm, string(publicKeyContent), replicationFactor)
+	} else if len(datacenterReplicationFactors) > 0 {
 		err = astarteAPIClient.Housekeeping.CreateRealmWithDatacenterReplication(realm, string(publicKeyContent),
 			datacenterReplicationFactors)
 	} else {
@@ -159,6 +287,106 @@ func realmsCreateF(command *cobra.Command, args []string) error {
 		os.Exit(1)
 	}
 
-	fmt.Println("ok")
+	fmt.Printf("Realm %s created successfully!\n", realm)
+
+	// If we're not creating a context, return
+	if !createContext {
+		return nil
+	}
+
+	realmContext := config.RealmConfiguration{
+		Name: realm,
+	}
+
+	if privateKeyContent != nil {
+		realmContext.Key = base64.StdEncoding.EncodeToString(privateKeyContent)
+	}
+
+	configContext := config.ContextFile{
+		Cluster: clusterConfigurationName,
+		Realm:   realmContext,
+	}
+
+	configDir := config.GetConfigDir()
+	if err := config.SaveContextConfiguration(configDir, contextName, configContext); err != nil {
+		fmt.Printf("Could not save cluster configuration: %s\n", err)
+		if privateKey == "" {
+			// Dump the private key
+			fmt.Println("Dumping private key for reference")
+			fmt.Println(string(privateKeyContent))
+		}
+	} else {
+		fmt.Printf("Context %s created successfully\n", contextName)
+
+		// Now set the current context to the new one
+		baseConfig, err := config.LoadBaseConfiguration(configDir)
+		if err != nil {
+			// Shoot out a warning, but don't fail
+			baseConfig = config.BaseConfigFile{}
+			fmt.Fprintf(os.Stderr, "warn: Could not load configuration file: %s. Will proceed creating a new one\n", err.Error())
+		}
+
+		baseConfig.CurrentContext = contextName
+		if err := config.SaveBaseConfiguration(configDir, baseConfig); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, "warn: Context not switched")
+		} else {
+			fmt.Printf("Context switched to %s\n", contextName)
+		}
+	}
+
 	return nil
+}
+
+func getPrivateKeyPEMBytes(key *rsa.PrivateKey) ([]byte, error) {
+	var pemkey = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+	var outbuf bytes.Buffer
+
+	if err := pem.Encode(&outbuf, pemkey); err != nil {
+		return nil, err
+	}
+
+	return outbuf.Bytes(), nil
+}
+
+func getPublicKeyPEMBytes(key *rsa.PublicKey) ([]byte, error) {
+	pkixBytes, err := x509.MarshalPKIXPublicKey(key)
+	if err != nil {
+		return nil, err
+	}
+	var pemkey = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pkixBytes,
+	}
+	var outbuf bytes.Buffer
+
+	if err := pem.Encode(&outbuf, pemkey); err != nil {
+		return nil, err
+	}
+
+	return outbuf.Bytes(), nil
+}
+
+func getClusterNameFromURLs() (string, error) {
+	configDir := config.GetConfigDir()
+	clusters, err := config.ListClusterConfigurations(configDir)
+	if err == nil {
+		for _, c := range clusters {
+			cluster, err := config.LoadClusterConfiguration(configDir, c)
+			if err != nil {
+				continue
+			}
+			if cluster.IndividualURLs.Housekeeping == viper.GetString("individual-urls.housekeeping") || cluster.URL == viper.GetString("url") {
+				// yay
+				return c, nil
+			}
+		}
+	} else {
+		return "", err
+	}
+	// Skip context creation
+	return "", errors.New("Not found")
 }
