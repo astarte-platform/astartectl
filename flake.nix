56bb1a3797a3d2c1cf7437c76c8ea731ca42d8cb
Francesco Noacco <francesco.noacco@secomind.com>
2023-06-05 17:30:49 +0200

    feat(nix): init nix dev shell
    Provide overlay and devshell with go package from `.tool-versions`
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Astarte command line client utility";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    go-utils = { url = "github:noaccOS/go-utils"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, go-utils, ... }:
+    flake-utils.lib.eachSystem go-utils.lib.defaultSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.${system}.default ];
+        };
+      in
+      {
+        overlays.default = go-utils.lib.asdfOverlay { src = ./.; };
+        overlay = self.overlays.default;
+        devShells.default = pkgs.simpleGoShell;
+      }
+    );
+}
